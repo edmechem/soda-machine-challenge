8b8acfcaaf95bc33fddcd66376b90147793ae8b5
Ed Mechem <ed@mechem.org>
2016-05-09 14:52:49 -0700

    Complete challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    soda = @sodas.select{ |item| item.brand == soda_brand}
+    soda = soda[0]
   end
 
   def sell(soda_brand)
+    soda = find_soda(soda_brand)
+    if soda != nil
+      @cash += soda.price
+      idx_to_remove = 0
+      @sodas.each_with_index do |item, index| 
+        if item.object_id == soda.object_id
+          idx_to_remove = index
+        end
+      end
+      sodas.delete_at(idx_to_remove)
+    end
   end
 
 end
